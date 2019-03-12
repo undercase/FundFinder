05da71aaa1d1c5fa0cda8f71961c4afeb0d0a88a
Thomas Hobohm <public@thomashobohm.com>
2019-03-12 15:59:24 -0500

    Fixed build directory in package.json.

---
+++ build/precache-manifest.4f9cefbeb78fbb84729e9a47bd44bbd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5796d6bfcea6b3b4f31",
+    "url": "/FundFinder/static/js/runtime~main.878e8433.js"
+  },
+  {
+    "revision": "1f5d87ddfef0c9006121",
+    "url": "/FundFinder/static/js/main.63ad68c9.chunk.js"
+  },
+  {
+    "revision": "e98545db596494f18a55",
+    "url": "/FundFinder/static/js/2.557896b5.chunk.js"
+  },
+  {
+    "revision": "1f5d87ddfef0c9006121",
+    "url": "/FundFinder/static/css/main.09512360.chunk.css"
+  },
+  {
+    "revision": "e942d6e3bfae15e854712b5aa1f7050e",
+    "url": "/FundFinder/index.html"
+  }
+];
